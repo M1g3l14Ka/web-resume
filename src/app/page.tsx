20ca26c92f997cdea72ef68682eed8c1fa3f0616
M1g3l14Ka <katcion00@mail.ru>
2026-04-12 01:30:00 +0300

    # Simplified: just HomePage wrapper now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,13 @@
 import HomePage from "@/components/HomePage";
-import ProjectsPage from "@/components/ProjectsPage";
-import WorkPage from "@/components/WorkPage";
-import FooterPage from "@/components/FooterPage";
-
-import { headerTitleBtns, headerTiles, hireBtns, petTimelineData, workTimelineData } from "@/data/data";
-
+import { headerTiles, petTimelineData } from "@/data/data";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col font-sans bg-[#050505]">
-
-      <section>
-        <HomePage
-          headerTitleBtns={headerTitleBtns}
-          headerTiles={headerTiles}
-          hireBtns={hireBtns}
-        />
-      </section>
-
-
-      <section id="Pet">
-        <ProjectsPage
-          petTimelineData={petTimelineData}
-        />
-      </section>
-
-      <section id="Work">
-        <WorkPage
-          workTimelineData={workTimelineData}
-        />
-      </section>
-
-      <FooterPage/>
-
-
-    </main>
+    <div className="relative">
+      <HomePage
+        headerTiles={headerTiles}
+        projects={petTimelineData}
+      />
+    </div>
   );
 }
