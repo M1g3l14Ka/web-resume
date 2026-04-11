b6fae99085fd6faf6e4a5213ce5478c7e3cb59b0
M1g3l14Ka <katcion00@mail.ru>
2026-04-11 20:20:12 +0300

    moved hero part in hero.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,22 +2,17 @@
 
 import Link from "next/link"
 import Image from "next/image"
-import { IHeaderTitleBtns, IHeaderTiles, ILinkBtns} from "@/types"
+import { IHeaderTitleBtns, IHeaderTiles} from "@/types"
 import { BorderTrail } from "@/components/motion-primitives/border-trail"
 import { motion, useScroll, useTransform } from "framer-motion"
-import { useMemo, useState, useSyncExternalStore } from "react"
-import HireForm from "@/components/HireForm"
-import AboutModal from "@/components/AboutMe"
+import { useMemo, useSyncExternalStore } from "react"
 
 interface HomePageProps {
     headerTitleBtns: IHeaderTitleBtns[];
     headerTiles: IHeaderTiles[];
-    hireBtns: ILinkBtns[];
 }
 
-export default function HomePage({ headerTitleBtns, headerTiles, hireBtns } : HomePageProps) {
-    const [isHireOpen, setIsHireOpen] = useState(false)
-    const [isAboutOpen, setIsAboutOpen] = useState(false)
+export default function HomePage({ headerTitleBtns, headerTiles }:HomePageProps) {
 
     const mounted = useSyncExternalStore(
         () => () => {},
@@ -86,49 +81,6 @@ export default function HomePage({ headerTitleBtns, headerTiles, hireBtns } : Ho
             </motion.div>
 
             <div className="flex flex-col p-2 z-10 w-auto h-full">
-                <div className="p-4 md:m-6 flex flex-row justify-between items-center w-auto">
-                    <div>
-                        <Link href="#">
-                            <h1 className="text-2xl md:text-4xl font-mono text-gray-600">Michael<span className="text-4xl font-bold text-purple-500">.</span></h1>
-                        </Link>
-                    </div>
-
-                    <div className="flex gap-4">
-                        <button
-                            type="button"
-                            className="font-mono font-bold text-lg md:text-2xl underline rounded-2xl px-4 py-1 md:w-32 hover:bg-linear-120 from-orange-400 to-rose-600/80 hover:scale-110 hover:border-none transition-all"
-                            onClick={() => setIsAboutOpen(true)}
-                        >
-                            About Me
-                        </button>
-
-                        {/* Кнопка Hire Me */}
-                        <button
-                            type="button"
-                            className="font-mono font-bold text-lg md:text-2xl underline rounded-2xl px-4 py-1 md:w-32 hover:bg-linear-120 from-orange-400 to-rose-600/80 hover:scale-110 hover:border-none transition-all"
-                            onClick={() => setIsHireOpen(true)}
-                        >
-                            Hire Me
-                        </button>
-                    </div>
-
-                    {isAboutOpen && (
-                        <AboutModal onClose={() => setIsAboutOpen(false)} />
-                    )}
-
-                    {/* Hire Me Modal */}
-                    {isHireOpen && (
-                        <div className="fixed inset-0 z-10000 flex items-center justify-center bg-[#050505] backdrop-blur-md p-4">
-                            <div className="relative flex flex-col w-full max-w-2xl">
-                                <HireForm
-                                    hireBtns={hireBtns}
-                                    toggleHireForm={() => setIsHireOpen(false)}
-                                />
-                            </div>
-                        </div>
-                    )}
-                </div>
-
                 <div className="flex-1 flex justify-center flex-col items-center md:m-24 w-auto">
                     <div>
                         <div className='relative w-56 h-72 md:w-72 md:h-96 rounded-2xl'>
@@ -149,13 +101,17 @@ export default function HomePage({ headerTitleBtns, headerTiles, hireBtns } : Ho
                     </div>
 
                     <div className="flex flex-col md:m-2 p-2 w-auto z-1000 items-center">
-                        <div className="text-5xl sm:text-7xl md:text-9xl lg:text-[12rem] flex justify-center items-center z-0 font-mono bg-clip-text text-transparent bg-linear-to-r from-orange-400 to-rose-600/70 w-auto leading-none text-center">
+                        <div className="text-5xl sm:text-7xl md:text-9xl lg:text-[12rem] flex justify-center items-center z-0 font-mono bg-clip-text 
+                            text-transparent bg-linear-to-r from-orange-400 to-rose-600/70 w-auto leading-none text-center"
+                        >
                             DEVELOPER
                         </div>
                         <div className="flex flex-wrap justify-center items-center gap-3 w-auto mt-6">
                             {headerTitleBtns.map((btn: IHeaderTitleBtns) => (
                                 <Link href={`#${btn.id}`} key={btn.id}>
-                                    <div className="flex justify-center items-center rounded-2xl border w-28 md:w-32 h-8 hover:bg-linear-120 from-orange-400 to-rose-600 transition ease-in-out duration-300 bg-[#131212] hover:scale-110 cursor-pointer hover:border-none">
+                                    <div className="flex justify-center items-center rounded-2xl border w-28 md:w-32 h-8 hover:bg-linear-120 from-orange-400
+                                      to-rose-600 transition ease-in-out duration-300 bg-[#131212] hover:scale-110 cursor-pointer hover:border-none"
+                                    >
                                         <button className="cursor-pointer text-xs md:text-base">
                                             {btn.label}
                                         </button>
